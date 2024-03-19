71b120e2f07959e039d41b44cf637ecbad391022
plida <85080905+plida@users.noreply.github.com>
2024-03-19 15:32:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def plot_sums(x, y, col, fg):
     fg.fill_between(x, 0, y, color=col, alpha=0.33)
 
 
-def plot_function(n, x, y, l, fg):
-    fg.plot(x, y, label=l, color='black')
+def plot_function(n, x, y, lab, fg):
+    fg.plot(x, y, label=lab, color='black')
     fg.set_title(f'n = {n}')
 
 
-def calculate_integral_sum(n, x, y, l, fg, xi_type):
+def calculate_integral_sum(n, x, y, lab, fg, xi_type):
     partitions = np.array_split(x, n)
     partitions_values = np.array_split(y, n)
     r = abs(partitions[0][0] - partitions[1][0])
@@ -33,43 +33,41 @@ def calculate_integral_sum(n, x, y, l, fg, xi_type):
                 value = partitions_values[i][-1]
             case "среднее":
                 lenpart = len(partitions_values[i])
-                value = partitions_values[i][lenpart//2]
+                value = partitions_values[i][lenpart // 2]
             case _:
                 value = np.random.choice(partitions_values[i])
-
         darbu = [value for x in range(len(partitions[i]))]
         plot_sums(partitions[i], darbu, colour, fg)
         summ += r * value
-    plot_function(n, x, y, l, fg)
+    plot_function(n, x, y, lab, fg)
     return summ
 
 
 def main():
-    # для вычисления интегралов
-    x, y, z = symbols('x y z')
+    x = symbols('x')  # для integrate
     var = input("\nВведите свой вариант: ")
     # выбираем вариант
     match var:
         case "17":
+            lab = "f(x) = x^2"
             r = [-1, 1]
             x_range = np.arange(r[0], r[-1], 0.001)
-            f = [x**2 for x in x_range]
-            lab = "f(x) = x^2"
-            intg = integrate(x**2, x)
-            intg = lambdify(x, intg)
+            f = [x ** 2 for x in x_range]
+            intg = integrate(x ** 2, x)
+            intg = lambdify(x, intg)  # первообразные
         case "21":
+            lab = "f(x) = 4^x"
             r = [0, 2]
             x_range = np.arange(r[0], r[-1], 0.001)
             f = [4 ** x for x in x_range]
-            lab = "f(x) = 4^x"
             intg = integrate(4 ** x, x)
             intg = lambdify(x, intg)
         case "28":
+            lab = "f(x) = cos(2x)"
             r = [0, math.pi]
             x_range = np.arange(r[0], r[-1], 0.001)
-            f = [math.cos(2*x) for x in x_range]
-            lab = "f(x) = cos(2x)"
-            intg = integrate(cos(2*x), x)
+            f = [math.cos(2 * x) for x in x_range]
+            intg = integrate(cos(2 * x), x)
             intg = lambdify(x, intg)
         case _:
             main()
@@ -95,4 +93,3 @@ def main():
 
 
 main()
-
